1db73f1bdd97f55a29a4824af708f3084ec89e5d
OZCAP <48883102+OZCAP@users.noreply.github.com>
2022-03-25 15:11:28 +0100

    🔧 Disable standalone output

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    outputStandalone: true,
+    outputStandalone: false,
   },
   reactStrictMode: true,
   images: {
