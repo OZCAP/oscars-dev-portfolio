5f76c5710943f3277d743083903c58f27abf484c
Oscar <oscar.pickerill@gmail.com>
2022-07-28 23:25:02 +0200

    💄 Change favicon to text Emoji

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -48,11 +48,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Head>
           <title>{"Oscar's Developer Portfolio"}</title>
           <meta name="description" content="Oscar Pickerill - Full-stack engineer portfolio: React, Typescript, Next JS, Node JS" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="apple-touch-icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡️</text></svg>" />
+          <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⚡️</text></svg>" />
           <link rel="manifest" href="/site.webmanifest"/>
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
           <meta name="apple-mobile-web-app-title" content="Oscar's Dev Portfolio"/>
           <meta name="application-name" content="Oscar's Dev Portfolio"/>
           <meta name="msapplication-TileColor" content="#da532c"/>
